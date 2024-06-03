569d7ca4c31286996aace18bc998d061887c8149
Aliencake <voloshko30@gmail.com>
2024-06-03 15:52:31 +0300

    init commit

---
+++ init.lua
@@ -0,0 +1,54 @@
+
+function OnModPreInit() 
+
+end
+
+function OnModInit() 
+
+end
+
+function OnModPostInit() 
+
+end
+
+function OnPlayerSpawned( player_entity ) 
+
+end
+
+function OnPlayerDied( player_entity ) 
+
+end
+
+function OnWorldInitialized() 
+
+end
+
+function OnWorldPreUpdate() 
+
+end
+
+function OnWorldPostUpdate() 
+
+end
+
+function OnBiomeConfigLoaded() 
+
+end
+
+function OnMagicNumbersAndWorldSeedInitialized() 
+
+end
+
+function OnPausedChanged( is_paused, is_inventory_pause ) 
+    ba
+
+end
+
+function OnModSettingsChanged() 
+
+end
+
+function OnPausePreUpdate() 
+
+end
+	
